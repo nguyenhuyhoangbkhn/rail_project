6da6c8e34ecc7da5339f8bd7e05eed8b3613d128
hoangnh <nguyenhuyhoangbkhn@gmail.com>
2016-03-26 09:49:27 +0000

    fix sign up

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,22 +1,2 @@
-class StaticPagesController < ApplicationController
-
-  def home
-    @title = "Home"
-  end
-
-  def help
-    @title = "Help"
-  end
-
-  def login
-    @title = "Log In"
-  end
-
-  def about
-    @title = "About"
-  end
-
-  def contact
-    @title = "Contact"
-  end
+module StaticPagesHelper
 end
