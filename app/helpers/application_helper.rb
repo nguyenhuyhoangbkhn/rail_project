ab13edb5cb0b0388b28d50713389833b0d132a89
Nguyễn Xuân Giáp <giapnx.ict@gmail.com>
2016-03-27 14:09:50 +0700

    update login + change password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
-   # Returns the full title on a per-page basis.
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
-  end
+	def full_title(page_title = '')
+		base_title = "旅行シェア"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,39 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  get 'password_resets/new'
 
-  get 'static_pages/about'
+  get 'password_resets/edit'
 
-  get 'static_pages/help'
+  get 'password_resets/new'
 
-  resources :users
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get 'users/new'
+
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy, :edit]
+  resources :relationships,       only: [:create, :destroy]
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
